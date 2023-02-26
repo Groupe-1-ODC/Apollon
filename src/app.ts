53abb9e3b20a568a38f6c9f55e46f92f227eb935
Corentin <corentin.lechene.pro@gmail.com>
2023-02-26 16:57:37 +0100

    feat(api)[19]: add nodemon

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import express from "express";
+import { Request, Response } from "express";
+const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
 	res.send("Hellow world");
 });
 
