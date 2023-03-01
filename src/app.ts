f609310313ff4951f6598b7ed00c6fdd6db60c72
Maram ZITOUNI <zitouni.maram20@gmail.com>
2023-03-01 18:20:24 +0100

    feat(api)[24]: add pg

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import Routers from "./app/index";
 
 const express = require("express");
 
+const db = require("./app/queries");
 
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+
+app.use(express.urlencoded({ extended: true }));
 app.use("/*", (req: Request, res: Response, next: NextFunction) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	console.log(`[${req.method}][${req.baseUrl}] => Requested`);
@@ -17,6 +19,9 @@ app.use("/*", (req: Request, res: Response, next: NextFunction) => {
 Routers.init(app);
 
 const PORT = 3000;
+
+app.get("/users", db.getUsers);
+
 app.listen(PORT, () => {
 	console.log(`Server listening at http://localhost:${PORT}`);
-})
+});
