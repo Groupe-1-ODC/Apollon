3247ef0888b4fff0b6240b73a42440bbfdafbb94
Corentin <corentin.lechene.pro@gmail.com>
2023-03-01 19:56:03 +0100

    feat(api)[24]: add postgresql

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
 import { Request, Response, NextFunction } from "express";
 import Routers from "./app/index";
 
+require('dotenv').config();
+
 const express = require("express");
 
-const db = require("./app/queries");
-
 const app = express();
+const PORT = process.env.API_PORT;
 
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
+
 app.use("/*", (req: Request, res: Response, next: NextFunction) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	console.log(`[${req.method}][${req.baseUrl}] => Requested`);
@@ -18,10 +19,6 @@ app.use("/*", (req: Request, res: Response, next: NextFunction) => {
 
 Routers.init(app);
 
-const PORT = 3000;
-
-app.get("/users", db.getUsers);
-
 app.listen(PORT, () => {
-	console.log(`Server listening at http://localhost:${PORT}`);
+	console.log(`Server listening at http://localhost:${process.env.API_PORT}`);
 });
