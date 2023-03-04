e4e8158953439a53272b8f79c5f4b02960cfad67
Maram ZITOUNI <zitouni.maram20@gmail.com>
2023-03-04 16:00:48 +0100

    feat(api)[18]:update roles routes

---
+++ src/app/roles/controllers.ts
@@ -0,0 +1,75 @@
+import Responses from "../../utils/Responses";
+import Role from "./model";
+import { Request, Response } from "express";
+
+const createRole = async (req: Request, res: Response): Promise<void> => {
+	try {
+		const { name } = req.body;
+		const role: Role = new Role(name);
+
+		const query = await role.insert();
+		Responses.Custom(res, query.rows);
+	} catch (err) {
+		console.error(err);
+		Responses.ErrorUnknown(res);
+	}
+};
+
+const fetchRoles = async (req: Request, res: Response): Promise<void> => {
+	try {
+		const roles = await Role.fetchAll();
+
+		Responses.Custom(res, roles.rows);
+	} catch (err) {
+		console.error(err);
+		Responses.ErrorUnknown(res);
+	}
+};
+
+const fetchRole = async (req: Request, res: Response): Promise<void> => {
+	try {
+		const { role_id } = req.params;
+		const role = await Role.fetchById(role_id);
+
+		Responses.Custom(res, role.rows);
+	} catch (err) {
+		console.error(err);
+		Responses.ErrorUnknown(res);
+	}
+};
+
+const fetchRoleByIsbn = async (req: Request, res: Response): Promise<void> => {
+	try {
+		const { isbn } = req.params;
+		const role = await Role.fetchByIsbn(isbn);
+
+		Responses.Custom(res, role.rows);
+	} catch (err) {
+		console.error(err);
+		Responses.ErrorUnknown(res);
+	}
+};
+
+const deleteRole = async (req: Request, res: Response): Promise<void> => {
+	try {
+		const { role_id } = req.params;
+		const query = await Role.deleteById(role_id);
+
+		if (query.rowCount === 0) {
+			Responses.NotFound(res);
+			return;
+		}
+		Responses.OK(res);
+	} catch (err) {
+		console.error(err);
+		Responses.ErrorUnknown(res);
+	}
+};
+
+module.exports = {
+	createRole,
+	deleteRole,
+	fetchRoleByIsbn,
+	fetchRole,
+	fetchRoles,
+};
