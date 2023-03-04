5077dc0fd637a42bb620e53475c028d812af2c64
Maram ZITOUNI <zitouni.maram20@gmail.com>
2023-03-04 16:00:48 +0100

    feat(api)[#18]: Add route /roles

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,13 +1,15 @@
-import {Express} from "express";
+import { Express } from "express";
 
-const RouterLib = require('./libs/routes');
-const RouterBook = require('./books/routes');
 const RouterUser = require('./users/routes');
+const RouterLib = require("./libs/routes");
+const RouterBook = require("./books/routes");
+const RouterRole = require("./books/roles");
 
 export default class Routers {
 	static init(app: Express): void {
 		app.use(RouterLib);
 		app.use(RouterBook);
 		app.use(RouterUser);
+		app.use(RouterRole);
 	}
 }
